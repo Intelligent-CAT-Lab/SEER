2d036372b2b730272aafd5103c7fa691375906a3
Anrath <aklounts@gmail.com>
2023-04-18 19:43:37 -0400

    results

--- real_data_gen/analyze_results.py
+++ real_data_gen/analyze_results.py
@@ -8,6 +8,7 @@ result_path = './real_data_gen/fold0/test_stats.csv'
 reader = csv.reader(open(result_path, 'r'))
 num_rows = -1
 num_acc = 0
+out_of_vocab = 0
 tp = 0
 fp = 0
 tn = 0
@@ -19,7 +20,9 @@ for row in reader:
     # if num_rows <= 68:
     #     recorded_rows.append(row)
     # f.write(recorded_rows[0])
-    if row[0] == '1' and row[1] == '1':
+    if row[0] == '-1':
+        out_of_vocab += 1
+    elif row[0] == '1' and row[1] == '1':
         tp += 1
     elif row[0] == '1' and row[1] == '0':
         fp += 1
@@ -35,14 +38,17 @@ for row in reader:
     #     f.write(f'row {num_rows} is different')
     #     break
 now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
-f = open(f'./{now}real_data_gen/results.txt', 'a')
+f = open(f'./real_data_gen/results.txt', 'a')
 # f.write(f'{sys.argv[1]}\n')
 f.write(f'num_rows: {num_rows}\n')
+f.write(f'out_of_vocab: {out_of_vocab}\n')
 f.write(f'tp: {tp}\n')
 f.write(f'fp: {fp}\n')
 f.write(f'tn: {tn}\n')
 f.write(f'fn: {fn}\n')
+f.write(f'Percent Out of Vocab: {out_of_vocab}/{num_rows} = {round((out_of_vocab)/num_rows, 4)}\n')
 f.write(f'accuracy: {tp+tn}/{num_rows} = {round((tp+tn)/num_rows, 4)}\n')
 f.write(f'precision: {tp}/{tp+fp} = {round(tp/(tp+fp), 4)}\n')
-f.write(f'recall: {tp}/{tp+fn} = {round(tp/(tp+fn), 4)}\n\n')
+f.write(f'recall: {tp}/{tp+fn} = {round(tp/(tp+fn), 4)}\n')
+f.write(f'f1: {2*tp}/{2*tp+fp+fn} = {round((2*tp)/(2*tp+fp+fn), 4)}\n\n')
 f.close()
