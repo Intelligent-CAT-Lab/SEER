614476ce570326ad6792968b7d2d41d4ee1728d7
anrath <aklounts@gmail.com>
2023-04-30 15:51:17 -0400

    results

--- real_data_gen/separate_vectorize_data.py
+++ real_data_gen/separate_vectorize_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from tqdm import tqdm
+import sys
 
 from json_to_h5 import json_to_h5
 
@@ -15,24 +16,4 @@ if __name__ == "__main__":
             model="JointEmbedder",
             comment_type=comment_type,
         )
-
-        df = pd.read_json(f"./real_data_gen/triplets/{comment_type}/triplets.json", orient="index")
-        projects = list(df["project"].unique())
-
-        print(f"generating json and h5 files for all projects...")
-        for project in tqdm(projects):
-            proj_data = df[df["project"] == project]
-            proj_data.to_json(
-                f"./real_data_gen/triplets/{comment_type}/triplets_{project}.json",
-                orient="index",
-                indent=4,
-            )
-            os.system(f"mkdir -p ./real_data_gen/fold0/{comment_type}/{project}")
-
-            json_to_h5(
-                type="test",
-                fold=0,
-                model="JointEmbedder",
-                project=project,
-                comment_type=comment_type,
-            )
+        
