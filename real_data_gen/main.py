9bc64cfadb81d86859ddf5df8f2f51410ee7bfb4
Anrath <aklounts@gmail.com>
2023-04-30 08:30:31 -0400

    working on comment robustness analysis

--- real_data_gen/main.py
+++ real_data_gen/main.py
@@ -5,15 +5,15 @@ import sys
 sys.path.append("./")
 from analyze_results import generate_results
 from overall_project_metrics import calculate_overall_metrics, combine_project_data
+from json_to_h5 import json_to_h5
 
 if __name__ == "__main__":
     os.system("pwd")
 
     if len(sys.argv) == 1:
-        # os.system('python3 ./real_data_gen/json_to_h5.py')
-        # os.system('python3 ./learning/test.py')
+        json_to_h5(type="test", fold=0, model="JointEmbedder")
+        os.system('python3 ./learning/test.py')
         generate_results(result_path=f"./real_data_gen/fold0/")
-        # os.system('python3 ./real_data_gen/analyze_results.py')
     else:
         if sys.argv[1] == "project":
 
@@ -22,19 +22,16 @@ if __name__ == "__main__":
             # projects = ['commons-imaging-1.0-alpha3-src', 'spark', 'commons-lang3-3.12.0-src', 'http-request', 'commons-geometry-1.0-src', 'springside4', 'commons-jexl3-3.2.1-src', 'joda-time', 'async-http-client', 'JSON-java', 'bcel-6.5.0-src', 'commons-weaver-2.0-src', 'commons-numbers-1.0-src', 'commons-collections4-4.4-src', 'jsoup', 'commons-jcs3-3.1-src', 'commons-validator-1.7', 'commons-net-3.8.0', 'commons-beanutils-1.9.4', 'commons-pool2-2.11.1-src', 'commons-rng-1.4-src', 'commons-configuration2-2.8.0-src', 'commons-vfs-2.9.0', 'scribejava', 'commons-dbutils-1.7']
 
             for project in projects:
-                # proj_data = df[df['project']==project]
-                # proj_data.to_json(f'./real_data_gen/triplets/triplets_{project}.json', orient='index', indent=4)
+                proj_data = df[df['project']==project]
+                proj_data.to_json(f'./real_data_gen/triplets/triplets_{project}.json', orient='index', indent=4)
                 # os.system(f'rm -rf ./real_data_gen/fold0/{project}')
-                # os.system(f'mkdir ./real_data_gen/fold0/{project}')
+                os.system(f'mkdir -p ./real_data_gen/fold0/{project}')
 
-                # os.system(f'python3 ./real_data_gen/json_to_h5.py {project}')
-                #     os.system(f'python3 ./learning/test.py --project {project}')
+                json_to_h5(type="test", fold=0, model="JointEmbedder", project=project)
+                os.system(f'python3 ./learning/test.py --project {project}')
                 generate_results(result_path=f"./real_data_gen/fold0/{project}/")
-                # os.system(f'python3 ./real_data_gen/analyze_results.py {project}')
-            # project_string = '*'.join(projects)
             combine_project_data(projects=projects)
             calculate_overall_metrics(projects=projects)
-            # os.system(f'python3 ./real_data_gen/overall_project_metrics.py {project_string}')
             generate_results(result_path=f"./real_data_gen/fold0/")
         elif sys.argv[1] == "coin":
             generate_results(
--- real_data_gen/modify_data.py
+++ real_data_gen/modify_data.py
@@ -2,6 +2,34 @@ import pandas as pd
 import os
 import re
 from tqdm import tqdm
+tqdm.pandas()
+
+def clean_text(df, remove_comments=True):
+    for col in ["C", "T"]:
+        df[col] = df[col].astype(str)
+        # Remove comments
+        if remove_comments:
+            df[col] = df.progress_apply(lambda row: re.sub(r"\s*\/\/.*\n", "", row[col].strip()), axis=1)
+        # Remove multiple spaces and line breaks
+        df[col] = df.progress_apply(lambda row: re.sub(r"\s\s*", " ", row[col].strip()), axis=1)
+
+    return df
+
+def apply_regex(df):
+    r_try = r"try\s*{"
+    r_fail = r"fail\([^;]*;\s*}"
+    r_except = r"catch\([^}]*\s*}"
+    try_except_regex = [r_try, r_fail, r_except]
+
+    for regex in tqdm(try_except_regex):
+        df["label"] = df.apply(
+            lambda row: "F" if (re.search(regex, row["T"]) != None) else row["label"],
+            axis=1,
+        )
+        df["T"] = df.apply(lambda row: re.sub(regex, "", row["T"]), axis=1)
+
+    return df
+
 
 folder_dir = "./toga_star"
 
@@ -24,27 +52,20 @@ for project in tqdm(folder_names):
 df["label"] = "P"
 df = df.drop("docstring", axis=1)
 df["T"] = df.apply(lambda row: re.sub(r"\s*assert.*", "", row["T"]), axis=1)
-for col in ["C", "T"]:
-    df[col] = df[col].astype(str)
-    # Remove comments
-    df[col] = df.apply(lambda row: re.sub(r"\s*\/\/.*\n", "", row[col].strip()), axis=1)
-    # Remove multiple spaces and line breaks
-    df[col] = df.apply(lambda row: re.sub(r"\s\s*", " ", row[col].strip()), axis=1)
+
+print("Cleaning text (0/6)...")
+df_comments = clean_text(df, remove_comments=False)
+print("Cleaning text (2/6)...")
+df_no_comments = clean_text(df, remove_comments=True)
 
 # REGEX
-r_try = r"try\s*{"
-r_fail = r"fail\([^;]*;\s*}"
-r_except = r"catch\([^}]*\s*}"
-
-try_except_regex = [r_try, r_fail, r_except]
-
-
 print("Applying REGEX...")
-for regex in tqdm(try_except_regex):
-    df["label"] = df.apply(
-        lambda row: "F" if (re.search(regex, row["T"]) != None) else row["label"],
-        axis=1,
-    )
-    df["T"] = df.apply(lambda row: re.sub(regex, "", row["T"]), axis=1)
+df_comments = apply_regex(df_comments)
+df_no_comments = apply_regex(df_no_comments)
 
-df.to_json("./real_data_gen/triplets/triplets.json", orient="index", indent=4)
+df_comments.to_json("./real_data_gen/triplets/triplets_comments.json", orient="index", indent=4)
+df_no_comments.to_json("./real_data_gen/triplets/triplets_no_comments.json", orient="index", indent=4)
+
+df_added_comments = df_no_comments.copy()
+df_added_comments['T'] = df_no_comments.apply(lambda row: row['T'][:-1] + "// Undeclared exception!" + row['T'][-1:], axis=1)
+df_added_comments.to_json("./real_data_gen/triplets/triplets_added_comments.json", orient="index", indent=4)
